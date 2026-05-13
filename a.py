b2d13c4167888edc3d27a8efe12e8021a4284b76
allan <zallan396@gmail.com>
2026-05-13 18:18:21 -0300

    Refactor tic-tac-toe game logic and validation

--- a.py
+++ a.py
@@ -1,4 +1,3 @@
-
 a = [     
         [1,2,3], #linha 0
         [4,5,6],
@@ -12,7 +11,53 @@ def mostrar_tabuleiro():
     print(f"{a[1][0]} | {a[1][1]} | {a[1][2]}")
     print("-" * 9)
     print(f"{a[2][0]} | {a[2][1]} | {a[2][2]}")
+
+def validar_jogada ():
+    if j1 > 9 or j1 < 1:
+      print("numero-invalido. Digite um numero de 1 a 9")
+    else:
+        pass
     
+def validar_jogada1 ():
+    if j2 > 9 or j2 < 1:
+      print("numero-invalido. Digite um numero de 1 a 9")
+    else:
+        pass
+    
+def verificar_vitoria ():
+     if a[0][0]  == "X" and a[0][1]  == "X" and a[0][2]  == "X" :
+         print("Vitoria do X")
+     elif a[1][0] == a[1][1] == a[1][2] == "X": #eu pedi para o chatgpt fazer essa parte pq seria so copiar e colar
+         print("Vitória do X")
+     elif a[2][0] == a[2][1] == a[2][2] == "X":
+         print("Vitória do X")
+     elif a[0][0] == a[1][0] == a[2][0] == "X":
+         print("Vitória do X")
+     elif a[0][1] == a[1][1] == a[2][1] == "X":
+         print("Vitória do X")
+     elif a[0][2] == a[1][2] == a[2][2] == "X":
+         print("Vitória do X")
+     elif a[0][0] == a[1][1] == a[2][2] == "X":
+         print("Vitória do X")
+     elif a[0][2] == a[1][1] == a[2][0] == "X":
+         print("Vitória do X")
+     elif a[0][0] == a[0][1] == a[0][2] == "O":
+        print("Vitória do O")
+     elif a[1][0] == a[1][1] == a[1][2] == "O":
+        print("Vitória do O")
+     elif a[2][0] == a[2][1] == a[2][2] == "O":
+        print("Vitória do O")
+     elif a[0][0] == a[1][0] == a[2][0] == "O":
+        print("Vitória do O")
+     elif a[0][1] == a[1][1] == a[2][1] == "O":
+        print("Vitória do O")
+     elif a[0][2] == a[1][2] == a[2][2] == "O":
+        print("Vitória do O")
+     elif a[0][0] == a[1][1] == a[2][2] == "O":
+        print("Vitória do O")
+     elif a[0][2] == a[1][1] == a[2][0] == "O":
+        print("Vitória do O")
+     
 def fazer_jogada(): 
     if j1 == 1:
        a[0][0] = "X"
@@ -55,21 +100,15 @@ def fazer_jogada1():
 
 while True:
  for i in range (8):
-    mostrar_tabuleiro()
+     mostrar_tabuleiro()
     
-    j1 = int(input("Digite o numero que quer troca por x:"))
-    fazer_jogada()
-    
-    mostrar_tabuleiro()
-    
-    j2 = int(input("Digite o numero que quer troca por O:"))
-
-# def validar_joga ():
-#     if j1 > 9 and j1 < 1:
-#       return numero-invalido
-#     elif j2 > 9 and j2 < 1:
-#       return numero invalido
-    
-
-# def verificar_vitoria ():
-#     if 
+     j1 = int(input("Digite o numero que quer troca por X:"))
+     validar_jogada()
+     fazer_jogada()
+     mostrar_tabuleiro()
+     verificar_vitoria()
+     j2 = int(input("Digite o numero que quer troca por O:"))
+     validar_jogada1()
+     fazer_jogada1()
+     verificar_vitoria()
+     
